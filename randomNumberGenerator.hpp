e6885bbd8875876fe17cf7480d0f614e7a16c895
l90lpa <liam1540@googlemail.com>
2016-07-28 14:16:58 -0700

    Header for random number generator

---
+++ randomNumberGenerator.hpp
@@ -0,0 +1,18 @@
+//  randomNumberGenerator.h
+//  First
+//
+//  Created by Liam on 21/12/2015.
+//  Copyright © 2015 Liam. All rights reserved.
+//
+
+#ifndef randomNumberGenerator_h
+#define randomNumberGenerator_h
+
+
+#include <cstdlib>
+#include <ctime>
+
+int randonNumberGenerator();
+
+
+#endif /* randomNumberGenerator_h */
